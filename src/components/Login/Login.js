1a65b3d7cbef8adc55b1dfeca558b79eb491e1aa
capmahin <mahinulhuque1996@gmail.com>
2022-04-09 15:41:44 +0600

    add navigator and locate it

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import { getAuth } from "firebase/auth";
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 import app from "../../firebase.init";
 import "./Login.css";
 
@@ -8,11 +9,21 @@ const auth = getAuth(app);
 
 const Login = () => {
   const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const from = location?.state?.from?.pathname || "/";
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle().then(() => {
+      navigate(from, { replace: true });
+    });
+  };
   return (
     <div>
       <h1>Login</h1>
       <div style={{ margin: "20px" }}>
-        <button onClick={() => signInWithGoogle()}>Google Sign In</button>
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
       </div>
       <form>
         <input type="email" placeholder="your email" />
